e375fbbbacb62e801583e69815023ddc3ab827eb
CLopMan <cesarlopman@gmail.com>
2023-12-21 10:13:23 +0100

    comentario 397

--- esMultiplo397.cpp
+++ esMultiplo397.cpp
@@ -3,6 +3,32 @@
 #include <iostream>
 #include <stdint.h>
 
+/*
+Primero debemos tener en cuenta el siguiente resultado: la suma de dos números consecutivos NO divisibles entre 3, será divisible entre 3. 
+
+Con esto en mente, es sencillo darse cuenta de que los números naturales seguirán el siguiente patrón respecto a su suma: 
+1 -> no se divide entre 3
+12 -> divisible entre 3
+123 -> divisible entre 3
+1234 -> no divisible entre 3
+12345 -> divisible entre 3
+123456 -> divisible entre 3
+1234567 -> no divisible entre 3
+
+Y así sucesivamente. 
+
+Cuando alcanzamos los números de dos cifras, seguimos el mismo patrón ya que 10 equivale a 1 (1 + 0 = 0), 11 a 2, 12 a 3... 19 a 1, 20 a 2, 21 a 3... 28 a 1, 29 a 2...
+
+Esto último, para verlo con más calma: 
+
+29 => 2 + 9 = 11. 11 mod 3 = 2
+
+Por tanto, tan sólo nos interesa saber si el conjunto de los N números (donde N es la entrada) se puede agrupar por tríos: 
+    - Si sobra 1 número => no es divisible
+    - Si sobran 2 personas o ninguna => es divisible. 
+
+*/
+
 bool is_divisible_by_3(uint_fast32_t n) {
     uint_fast32_t aux = n%3;
     if (aux == 0) return true;
